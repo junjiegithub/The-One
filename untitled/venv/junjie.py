dfe2694b983f36fbbdff8cd193ac46dc3f2a3883
junjiegithub <1416279053@qq.com>
2019-11-04 17:54:16 +0800

    first commit

---
+++ untitled/venv/junjie.py
@@ -0,0 +1,43 @@
+#
+
+#函数，给一串动作起名字
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+#
+# number = random.randint(1,6)
+# print(number)
+#
+# #摇色子游戏
+# while 1:
+#  getNum=int(input('请输入你猜测的数字：'))
+#  if getNum==number:
+#   print('恭喜你猜对了！奖励你20元现金')
+#   break
+#  elif getNum>number:
+#   print('很遗憾！你猜的数字大了，扣除你1元现金')
+#  else:
+#   print('很遗憾!你猜的数字小了，扣除你1元现金')
